d3f4275a1ecdb1f0b5bc74aeea544baae28cb9d0
Rudra swain <rudrasankar96@gmail.com>
2025-06-28 20:53:06 +0530

    Adding more js concept

--- Resturant-Menu/script.js
+++ Resturant-Menu/script.js
@@ -1,3 +1,17 @@
 function showThankYou(){
     alert("Thanks for checking our menu");
+
+  const sections = document.querySelectorAll(".menu-section");
+  
+  sections.forEach((section) => {
+    section.style.backgroundColor = "#ffe1b2"; // light orange
+    section.style.transition = "background-color 0.5s ease";
+  });
+  
+  // after 2 seconds wapas normal
+  setTimeout(() => {
+    sections.forEach((section) => {
+      section.style.backgroundColor = "#fbeedc"; // original color
+    });
+  }, 2000);
 }
