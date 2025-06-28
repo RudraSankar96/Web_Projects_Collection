5033d323880dbf0c1904504dc99bd204fa5f8f58
Rudra swain <rudrasankar96@gmail.com>
2025-06-28 21:23:10 +0530

    update some thing

--- Resturant-Menu/script.js
+++ Resturant-Menu/script.js
@@ -1,3 +1,22 @@
+// script.js me add karo
+
+let sliderImages = [
+  "https://media.istockphoto.com/id/1340754104/photo/closeup-flat-top-macro-of-fresh-raw-chopped-vegetable-salad-with-romaine-lettuce-greens-sweet.jpg?s=612x612&w=0&k=20&c=w260XDt_7HO4j5uMZvDDIr-ov17xMdjeVwpst5Ct_Sg=",
+  "https://media.istockphoto.com/id/1401745103/photo/fried-chicken-wings.jpg?s=612x612&w=0&k=20&c=lhuKzqBPqng3zG_euXFSVcf7uDRguu4jC0OScKw9Rhg=",
+  "https://media.istockphoto.com/id/1365480248/photo/chocolate-muffins.jpg?s=612x612&w=0&k=20&c=O2vVdpsHK83T0skY0uIh6c8Lx3RQEsYSp-VyAlxAEmk="
+];
+
+let currentIndex = 0;
+
+function changeSlider() {
+  const sliderImg = document.getElementById("sliderImage");
+  sliderImg.src = sliderImages[currentIndex];
+  currentIndex = (currentIndex + 1) % sliderImages.length;
+}
+
+// change image every 3 sec
+setInterval(changeSlider, 3000);
+
 function showThankYou(){
     alert("Thanks for checking our menu");
 
