1323c60e542227be7335011892b94820c4f77513
Rudra swain <rudrasankar96@gmail.com>
2025-06-30 23:03:41 +0530

    adding extra functionality

--- Cafe_Menu/script.js
+++ Cafe_Menu/script.js
@@ -1,10 +1,15 @@
-// script.js
-
-// Wait until the DOM is ready
 document.addEventListener("DOMContentLoaded", function() {
   const websiteLink = document.querySelector("footer a");
 
   websiteLink.addEventListener("click", function() {
     alert("Thanks for visiting Camper Cafe!");
+  });
+
+  const items = document.querySelectorAll(".flavor, .dessert");
+
+  items.forEach(function(item) {
+    item.addEventListener("click", function() {
+      console.log(`You clicked ${item.textContent}!`);
+    });
   });
 });
