5f082ba7e26cb10694bea79290d7b8f68f781c59
Rudra swain <rudrasankar96@gmail.com>
2025-07-02 22:47:08 +0530

    create a sample project

---
+++ project_sample/script.js
@@ -0,0 +1,13 @@
+function login() {
+  // basic fake login validation
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  
+  if (user === "admin" && pass === "admin") {
+    window.location.href = "dashboard.html";
+    return false;
+  } else {
+    alert("Invalid username or password. Try 'admin'/'admin'");
+    return false;
+  }
+}
