46339e6b554a0279abf66e027a5a32b8929cef8d
Rudra swain <rudrasankar96@gmail.com>
2025-08-12 22:17:06 +0530

    add the bird functionality

--- flappy_bird_clone/script.js
+++ flappy_bird_clone/script.js
@@ -3,3 +3,11 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 400;
 canvas.height = 500;
+
+// Bird
+let birdX = 50;
+let birdY = 150;
+let birdSize = 20;
+let gravity = 1.5;
+let lift = -20;
+let velocity = 0;
