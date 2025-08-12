37756c928aac0208953c920fe9aa3cacddd56b3c
Rudra swain <rudrasankar96@gmail.com>
2025-08-12 22:19:29 +0530

    adding pipes

--- flappy_bird_clone/script.js
+++ flappy_bird_clone/script.js
@@ -11,3 +11,13 @@ let birdSize = 20;
 let gravity = 1.5;
 let lift = -20;
 let velocity = 0;
+
+// Pipes
+let pipes = [];
+let pipeWidth = 50;
+let pipeGap = 120;
+let pipeSpeed = 2;
+
+// Score
+let score = 0;
+let gameOver = false;
