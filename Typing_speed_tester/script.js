6739bf5df954cbd8ca34ff5a1b53364f1dae18d4
Rudra swain <rudrasankar96@gmail.com>
2025-08-25 20:48:28 +0530

    Adding fucntionality to the page

---
+++ Typing_speed_tester/script.js
@@ -0,0 +1,7 @@
+const quotes = [
+  "The quick brown fox jumps over the lazy dog.",
+  "Typing fast is not as important as typing accurately.",
+  "JavaScript makes the web alive with interactivity.",
+  "Stay consistent and success will follow.",
+  "Coding is like humor. If you have to explain it, it’s bad."
+];
